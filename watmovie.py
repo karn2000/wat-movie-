570d003dd0a1990ce1d0434e6355b5b23876d4d1
worravej <s7070104@kmitl.ac.th>
2014-12-10 12:52:27 +0700

    changelabel
    :D

--- watmovie.py
+++ watmovie.py
@@ -2,18 +2,20 @@ from Tkinter import *
 
 def d_movie(label2):
     movie = ment.get()
-    movie_list = {'All':['com1','com2','drama1','drama2','Act1','Act2','Sci1','Sci2']\
+    movie_list = {'All':['com1','com2','drama1','drama2','act1','act2','sci1','sci2']\
                       ,'Comedy':['com1','com2']\
                       ,'Drama' :['drama1','drama2']\
-                      ,'Action':['Act1','Act2']\
-                      ,'Sci-fi':['Sci1','Sci2']}
+                      ,'Action':['act1','act2']\
+                      ,'Sci-fi':['sci1','sci2']}
     check = 0
+    movie_1 = ''
     for i in movie_list:
         if i == option1.get():
             for l in movie_list[i]:
                 if movie.lower() in l:
+                    movie_1 += l + '\n'
                     check += 1
-                    changelabel(label2, l)
+                    changelabel(label2, movie_1)
     if check == 0:
         text = 'Not Found'
         changelabel(label2, text)
@@ -28,7 +30,7 @@ root.title('Wat Movie ??')
 root.geometry('300x300')
 root.resizable(0, 0)
 
-l_option = ['All', 'Comedy', 'Action', 'Sci-fi']
+l_option = ['All', 'Comedy', 'Drama', 'Action', 'Sci-fi']
 option1 = StringVar()
 option1.set(l_option[0])
 ment = StringVar()
