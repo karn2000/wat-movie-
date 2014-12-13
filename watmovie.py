d890a59e8b178f06adb7f6da21e06e3bdab9cb31
kindaichi <s7070098@kmitl.ac.th>
2014-12-13 22:38:57 +0700

    Revert "Revert "pic""
    This reverts commit 2425c7ef01c2d67682feead97389b3d56acce50f.

--- watmovie.py
+++ watmovie.py
@@ -1,17 +1,19 @@
 ''' Wat Movie ?? '''
+import sys
 from Tkinter import *
 import os
 import tkMessageBox
 
-def d_movie(label2):
+def d_movie(label2, ment, option1):
     ''' find movie '''
     movie = ment.get()
-    if len(movie) < 3:
+    if len(movie) < 3 or movie == 'the':
         movie += '   '
-    movie_list = {'All':['interstellar','com1','com2','drama1','drama2','act1','act2','sci1','sci2']\
+    movie_list = {'All':['com1','com2','drama1','drama2','act1','act2','sci1','sci2', 'interstellar', 'The hobbit 3']\
                       ,'Comedy':['com1','com2']\
                       ,'Drama' :['drama1','drama2']\
                       ,'Action':['act1','act2']\
+                      ,'Advanture':['the hobbit 3']\
                       ,'Sci-fi':['sci1','sci2', 'interstellar']}
     check = 0
     movie_1 = ''
@@ -19,9 +21,12 @@ def d_movie(label2):
         if i == option1.get():
             for l in movie_list[i]:
                 if movie.lower() in l:
-                    movie_1 += l
                     check += 1
-                    changelabel(label2, movie_1)
+                    if check > 1:
+                        movie_1 += '\n'
+                    movie_1 += l
+            if check >= 1:
+                changelabel(label2, movie_1)
                     
     if check == 0:
         text = 'Not Found'
@@ -30,12 +35,11 @@ def d_movie(label2):
 
 def changelabel(before, after):
     ''' change label when click search '''
-    before.config(text=after)
-    movie = str(after + '.gif')
-    print movie
-    img = PhotoImage(file=movie)
-    label = Label(root, image=img).pack()
-
+    before.config(text=after.capitalize())
+    movie = after.lower()+'.gif'
+    ooo = PhotoImage(file = movie)
+    label = Label(image=ooo).place(relx=.69, rely=.57)
+    pic(ooo)
 
 def about_us():
     ''' open file => about_us.txt '''
@@ -51,46 +55,52 @@ def mquit():
     if mexit > 0:
         root.destroy()
 
-root = Tk()
-root.title('Wat Movie ??')
-root.geometry('500x500')
-root.resizable(0, 0)
+class main(object):
+    def __init__(self):
+        self.root = Tk()
+        self.root.title('Wat Movie ??')
+        self.root.geometry('500x500')
+        self.root.resizable(0, 0)
 
-# logo
-top = PhotoImage(file = 'top.gif')
-labeltop = Label(root, image=top).pack()
+        # logo
+        top = PhotoImage(file = 'top.gif')
+        labeltop = Label(self.root, image=top).pack()
 
-# background
-photo = PhotoImage(file='bg2.gif')
-label_pic = Label(root, image=photo).place(relx=.5, rely=.14, anchor="n")
+        # background
+        photo = PhotoImage(file='bg2.gif')
+        label_pic = Label(self.root, image=photo).place(relx=.5, rely=.14, anchor="n")
 
-# category
-l_option = ['All', 'Comedy', 'Drama', 'Action', 'Sci-fi']
-option1 = StringVar()
-option1.set(l_option[0])
-ment = StringVar()
+        # category
+        l_option = ['All', 'Advanture', 'Action', 'Comedy', 'Drama', 'Sci-fi']
+        option1 = StringVar()
+        option1.set(l_option[0])
+        ment = StringVar()
 
-label2 = Label(root, text = '---', bg = 'black', fg = 'white', font = 15)
-label2.place(relx=.5, rely=.55, anchor = 'c')
+        label2 = Label(self.root, text = '---', bg = 'black', fg = 'white', font = 15)
+        label2.place(relx=.5, rely=.55, anchor = 'c')
 
-label_category = Label(root, bg='#5fbfc5', text = 'Category   : ').place(relx=.44, rely=.3, anchor= 'c')
+        label_category = Label(self.root, bg='#5fbfc5', text = 'Category   : ').place(relx=.4, rely=.3, anchor= 'c')
 
-m_entry = Entry(root, textvariable = ment).place(relx=.5, rely=.2, anchor = 'c')
-m_option = apply(OptionMenu,(root, option1) + tuple(l_option)).place(relx=.57, rely=.3, anchor= 'c')
-m_search = Button(root, text = 'SEARCH', bg='#5fbfc5',command = lambda : d_movie(label2)).place(relx=.5, rely=.4, anchor= 'c')
+        m_entry = Entry(self.root, textvariable = ment).place(relx=.5, rely=.2, anchor = 'c')
+        m_option = apply(OptionMenu,(self.root, option1) + tuple(l_option)).place(relx=.57, rely=.3, anchor= 'c')
+        m_search = Button(self.root, text = 'SEARCH', bg='#5fbfc5',command = lambda : d_movie(label2, ment, option1)).place(relx=.5, rely=.4, anchor= 'c')
 
 
-# menubar
-menubar = Menu(root)
-# help menu
-helpmenu = Menu(menubar,tearoff=0)
-helpmenu.add_command(label='View Help', command=wmhelp)
-helpmenu.add_command(label='About Us',command=about_us)
-menubar.add_cascade(label='Help',menu=helpmenu)
+        # menubar
+        menubar = Menu(self.root)
+        # help menu
+        helpmenu = Menu(menubar,tearoff=0)
+        helpmenu.add_command(label='View Help', command=wmhelp)
+        helpmenu.add_command(label='About Us',command=about_us)
+        menubar.add_cascade(label='Help',menu=helpmenu)
 
-quit_button = Button(root, text = 'Close',bg = 'pink',font=15, command = mquit).place(relx=.1, rely=.9, anchor='c')
+        quit_button = Button(self.root, text = 'Close',bg = 'pink',font=15, command = mquit).place(relx=.1, rely=.9, anchor='c')
 
-root.config(menu=menubar)
-root.mainloop()
+        self.root.config(menu=menubar)
+        self.root.mainloop()
+
+
+main()
+
 
 
