48b619fdc5ccbf28da3d940c20a1068def5a966e
Worravej <s7070104@kmitl.ac.th>
2014-12-13 15:28:47 +0700

    bg
    bg

--- watmovie.py
+++ watmovie.py
@@ -1,7 +1,10 @@
 from Tkinter import *
 
+
 def d_movie(label2):
     movie = ment.get()
+    if len(movie) < 3:
+        movie += '   '
     movie_list = {'All':['com1','com2','drama1','drama2','act1','act2','sci1','sci2']\
                       ,'Comedy':['com1','com2']\
                       ,'Drama' :['drama1','drama2']\
@@ -16,6 +19,7 @@ def d_movie(label2):
                     movie_1 += l + '\n'
                     check += 1
                     changelabel(label2, movie_1)
+                    
     if check == 0:
         text = 'Not Found'
         changelabel(label2, text)
@@ -24,26 +28,31 @@ def d_movie(label2):
 def changelabel(before, after):
     before.config(text=after)
 
+
 root = Tk()
 root.title('Wat Movie ??')
 root.geometry('500x500')
 root.resizable(0, 0)
+
+top = PhotoImage(file = 'top.gif')
+labeltop = Label(root, image=top).pack()
+
+photo = PhotoImage(file='bg2.gif')
+label_pic = Label(root, image=photo).place(relx=.5, rely=.14, anchor="n")
 
 l_option = ['All', 'Comedy', 'Drama', 'Action', 'Sci-fi']
 option1 = StringVar()
 option1.set(l_option[0])
 ment = StringVar()
 
-label1 = Label(root, text='Search Movie and Choose Categories').pack()
-label2 = Label(root, text = 'Wat Movie??')
+#label1 = Label(root, text='Search Movie and Choose Categories').place(relx=.5, rely=.1, anchor="c")
+label2 = Label(root, text = 'Movie', bg = 'black', fg = 'white', size = '15')
 
-m_entry = Entry(root, textvariable = ment).pack()
-m_search = Button(root, text = 'SEARCH', command = lambda : d_movie(label2)).pack()
-m_option = apply(OptionMenu,(root, option1) + tuple(l_option)).pack()
-label2.pack()
+m_entry = Entry(root, textvariable = ment).place(relx=.5, rely=.2, anchor="c")
+m_search = Button(root, text = 'SEARCH', command = lambda : d_movie(label2)).place(relx=.5, rely=.3, anchor="c")
+m_option = apply(OptionMenu,(root, option1) + tuple(l_option)).place(relx=.5, rely=.4, anchor="c")
+label2.place(relx=.5, rely=.5, anchor="c")
 
-photo = PhotoImage(file='imm.gif')
-label_pic = Label(root, image=photo).pack()
 
 
 root.mainloop()
